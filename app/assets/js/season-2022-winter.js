f6be45993559bd2d40e42792eb75c33296a2b544
Alexander German <gitaristium@gmail.com>
2022-11-25 01:03:19 +0700

    закончил две страницы сезонки
    апельсин и хурма

--- app/assets/js/season-2022-winter.js
+++ app/assets/js/season-2022-winter.js
@@ -41,22 +41,49 @@ $(document).ready(function () {
 
 
   //паралакс при скролле
-  if ('.fixed-parallax-items') {
+  if ($('.fixed-parallax-items').length) {
     var vw = $(window).height() / 100;
 
     $(window).on("scroll", function () {
       let scroll = $(window).scrollTop() / vw;
 
-      if (scroll <= 100) {
-        $('.fixed-parallax-items.fixed-parallax-items-h-l').css({
-          'left': (-scroll - 17) + 'vw'
-        });
-        $('.fixed-parallax-items.fixed-parallax-items-h-r').css({
-          'right': (-scroll - 22) + 'vw'
-        });
+      if ($('.section-one__orange').length) {
+        if (scroll <= 100) {
+          $('.section-one__orange.fixed-parallax-items.fixed-parallax-items-h-l').css({
+            'left': (-scroll - 17) + 'vw'
+          });
+          $('.section-one__orange.fixed-parallax-items.fixed-parallax-items-h-r').css({
+            'right': (-scroll - 22) + 'vw'
+          });
+        }
+      }
+      if ($('.section-one__hurma').length) {
+        if (scroll <= 100) {
+          $('.section-one__hurma.fixed-parallax-items.fixed-parallax-items-h-l').css({
+            'left': (-scroll - 24) + 'vw',
+            'top': (-scroll + 52) + 'vw'
+          });
+          $('.section-one__hurma.fixed-parallax-items.fixed-parallax-items-h-r').css({
+            'right': (-scroll - 9) + 'vw',
+            // 'top': (-scroll - 5) + 'vw'
+          });
+        }
+      }
+
+      if ($('.wrapper__bg-line').length) {
+        let scrollLine = $(document).scrollTop() / ($(document).height() - $(window).height()) * 6000;
+
+        if (scrollLine <= 5500) {
+          $('.wrapper__bg-line').css({
+            'stroke-dasharray': scrollLine + ' ' + (8000 - scrollLine)
+          });
+        }
+        else {
+          $('.wrapper__bg-line').css({
+            'stroke-dasharray': (scrollLine + (scrollLine - 5500) * 4) + ' ' + (8000 - (scrollLine + (scrollLine - 5500) * 4))
+          });
+        }
       }
     });
   }
-
-
 });
